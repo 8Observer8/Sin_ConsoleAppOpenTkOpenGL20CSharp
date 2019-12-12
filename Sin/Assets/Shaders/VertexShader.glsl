d404d215aeea547b78acdd07370247e55902facf
Ivan Enzhaev <8observer8@gmail.com>
2019-12-12 23:44:53 +0400

    first commit

---
+++ Sin/Assets/Shaders/VertexShader.glsl
@@ -0,0 +1,11 @@
+﻿#version 110
+
+attribute vec2 position;
+
+uniform mat4 projectionMatrix;
+uniform mat4 viewMatrix;
+
+void main()
+{
+	gl_Position = projectionMatrix * viewMatrix * vec4(position, 0.0, 1.0);
+}
